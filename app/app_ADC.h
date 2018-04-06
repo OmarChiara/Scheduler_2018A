6c80d7b0c801d5e4dbfb3891b842fc40bc1f711e
Misael <Misael@Chiara-pc>
2018-04-05 19:40:56 -0500

    adc added

---
+++ app/app_ADC.h
@@ -0,0 +1,14 @@
+/*
+ * app_ADC.h
+ *
+ *  Created on: Apr 5, 2018
+ *      Author: Misael
+ */
+
+#ifndef APP_ADC_H_
+#define APP_ADC_H_
+
+extern void app_ADC_Init(void);
+extern void app_ADC_Task(void);
+
+#endif /* APP_ADC_H_ */
